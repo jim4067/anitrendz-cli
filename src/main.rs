a2f3630c1f422c075a0b9e1e4bfe10478a25c3ca
jim4067 <jimmyimpulse2@gmail.com>
2021-06-30 01:39:53 +0300

    feat: moved data into repective crates

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,22 @@
 extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
+extern crate tokio;
 
-use std::thread::sleep;
-use std::time::Duration;
-
-use std::fs::File;
-use std::io::Read; //this was the be all end all line that was missing
-
-use serde::*;
-
-#[derive(Debug, Serialize, Deserialize)]
-#[allow(non_snake_case)]
-struct Response {
-    code: i32,
-    message: String,
-    totalItems: i32,
-    data: Vec<Anime>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Stats {
-    peak: i32,
-    //previously: i32, //there is a "-" so find out how to fix it
-    stat: String,
-    status: String, //consider making this an enum value
-    weeksOnTop: i32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[allow(non_snake_case)]
-struct Anime {
-    title: String,
-    imageUrl: String,
-    studio: String,
-    stats: Stats,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Foo {
-    name: String,
-    age: u32,
-}
+mod network;
+mod app;
 
 fn main() {
-    let delay = Duration::from_millis(300); //for fun
-
-    let mut file = File::open("topanime.json").unwrap();
-    let mut buff = String::new();
-    file.read_to_string(&mut buff).unwrap();
-
-    let res: Response = serde_json::from_str(&buff).unwrap();
-    // println!("{:#?}", res.data[3]);
-    let data = res.data;
-    for item in data.iter() {
-        println!("{:?}", item);
-        sleep(delay); //for fun
-    }
+    // println!("let us print the title of the top anime");
+    // for anime in data.iter() {
+    //     println!(" image {}", anime.imageUrl);
+    //     println!(" peak {}", anime.stats.peak);
+    //     println!(" previously {}", anime.stats.previously);
+    //     println!(" stat {}", anime.stats.stat);
+    //     println!(" status {}", anime.stats.status);
+    // }
+    app::top_anime_list();
 }
 
 //see if when i get the data I can sterilize it and change some of the variable names to use snake case
+//you could add a rank field and learn how to calculate it
