70aabf14339bdf966659aa6cddf169eda8549bad
jim4067 <jimmyimpulse2@gmail.com>
2021-07-03 21:50:11 +0300

    :contrutcion: :tada: finish this crate :tada:

--- src/app.rs
+++ src/app.rs
@@ -1,39 +1,16 @@
 #![allow(non_snake_case, dead_code)]
 
 use rand::Rng;
-use std::f64::consts::PI;
 
 use crate::anime_net;
 use crate::music_net;
-
-fn rgb(freq: f64, spread: f64, i: f64) -> (u8, u8, u8) {
-    let j = i / spread;
-    let red = (freq * j + 0.0).sin() * 127.0 + 128.0;
-    let green = (freq * j + 2.0 * PI / 3.0).sin() * 127.0 + 128.0;
-    let blue = (freq * j + 4.0 * PI / 3.0).sin() * 127.0 + 128.0;
-
-    (red as u8, green as u8, blue as u8)
-}
-
-fn rainbow_println(line: &str, frequency: f64, spread: f64) {
-    for (i, c) in line.char_indices() {
-        let (r, g, b) = rgb(frequency, spread, i as f64);
-        print!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, c);
-    }
-    println!();
-}
+use crate::utils::rainbow_println;
 
 pub fn top_anime_list() {
     let res = anime_net::get_anime();
     let data = res.data;
 
     for (pos, anime) in data.iter().enumerate().rev() {
-        // println!("title: {}", anime.title);
-        // println!("image{}", anime.imageUrl);
-        // println!("studio: {}", anime.studio);
-        // println!("rank: {}", pos + 1);
-        // println!("{:?}", anime.stats);
-        // println!();
         rainbow_println(format!(" title: {}", anime.title).as_str(), 0.1, 3.0);
         rainbow_println(format!(" studio: {}", anime.studio).as_str(), 0.1, 3.0);
         rainbow_println(format!(" rank: {}", pos + 1).as_str(), 0.1, 3.0);
@@ -52,45 +29,12 @@ pub fn top_anime_list() {
         );
         println!();
     }
-
-    // offline testing
-    // let res = anime_net::offline_test();
-    // let data = res.data;
-
-    // for (pos, anime) in data.iter().enumerate().rev() {
-    //     rainbow_println(format!(" title: {}", anime.title).as_str(), 0.1, 3.0);
-    //     rainbow_println(format!(" studio: {}", anime.studio).as_str(), 0.1, 3.0);
-    //     rainbow_println(format!(" rank: {}", pos + 1).as_str(), 0.1, 3.0);
-    //     rainbow_println(format!(" {:?}", anime.stats).as_str(), 0.1, 3.0); //stats for nerds. impl Display and use a bunch of prints
-    //     println!();
-    // }
-}
-
-pub fn top_anime_list_uncolored() {
-    let res = anime_net::get_anime();
-    let data = res.data;
-
-    for (pos, anime) in data.iter().enumerate().rev() {
-        println!(" title: {}", anime.title);
-        println!(" studio: {}", anime.studio);
-        println!(" rank: {}", pos + 1);
-        println!(
-            " stats for nerds : [ peak: {} | previously: {} | stat: {} | status: {} | weeks_on_top: {} ] ",
-            anime.stats.peak,
-            anime.stats.previously,
-            anime.stats.stat,
-            anime.stats.status,
-            anime.stats.weeksOnTop
-        );
-        println!();
-    }
 }
 
 pub fn music_charts_list() {
-    let res = music_net::get_music(); //if using result, i'd call result here
+    let res = music_net::get_music(); //if using result, i'd call unwrap here
     let data = res.data;
 
-    // println!("{:#?}", data);
     for song in data.iter().rev() {
         rainbow_println(format!(" title: {}", song.title).as_str(), 0.1, 3.0);
         rainbow_println(format!(" artists: {:?}", song.artists).as_str(), 0.1, 3.0);
@@ -151,45 +95,7 @@ pub fn music_charts_list() {
     }
 }
 
-pub fn music_charts_list_uncolored() {
-    let res = music_net::get_music();
-    let data = res.data;
-
-    for song in data.iter().rev() {
-        println!(" title: {}", song.title);
-        println!(" studio: {:?}", song.artists);
-        println!(" rank: {}", song.rank);
-        println!(
-            " stats for nerds :: [ peak: {} | previously: {} | weeks_on_top: {} ]",
-            song.stats.peak, song.stats.previously, song.stats.weeks
-        );
-        println!(
-            " listen: youtube -> {}",
-            song.mediaUrl
-                .youtube
-                .as_ref()
-                .unwrap_or(&"404 Not Available".to_string())
-        );
-        println!(
-            " \t spotify -> {}",
-            song.mediaUrl
-                .spotify
-                .as_ref()
-                .unwrap_or(&"404 Not Available".to_string())
-        );
-        println!(
-            " \t itunes  -> {}",
-            song.mediaUrl
-                .itunes
-                .as_ref()
-                .unwrap_or(&"404 Not Available".to_string())
-        );
-        println!();
-    }
-}
-
 //generating random anime to watch
-//would you like a random recommendation
 pub fn gen_random_anime() {
     let res = anime_net::get_anime();
 
@@ -221,7 +127,11 @@ pub fn gen_random_song() {
     let num = rand::thread_rng().gen_range(0..res.totalItems) as usize;
 
     let song = &res.data[num];
-    rainbow_println("\n A random recommendation a song to listen to \n", 0.1, 3.0);
+    rainbow_println(
+        "\n A random recommendation of a song to listen to \n",
+        0.1,
+        3.0,
+    );
 
     rainbow_println(format!(" title: {}", song.title).as_str(), 0.1, 3.0);
     rainbow_println(format!(" artists: {:?}", song.artists).as_str(), 0.1, 3.0);
@@ -275,24 +185,5 @@ pub fn gen_random_song() {
     );
 }
 
-//for the cli application. only this will be required in main
-fn run() {}
-
 //this crate is responsible for printing to the cli
-//anitrendz-cli anitop api but with fearless concurrency on the terminal
-
-//do not forget that you were to add the option to search for anime using the anilist free api
-//add cli option to either rainbow print or print basic. i,e green -> basic print
-
-//how many items do you want to be displayed
-
-// -a --anime -for listing the anime. subcommand to either rainbow print or basic print
-// -m --music-charts for listing the music charts. subcommand to either rainbow print or...
-// randomly recommend anime to user
-// randomly recommend song to user
-
-//what I have learned
-// using serde.js to deserialize and serialize json data
-//How to format and display custom types
-//ANSI excape code for the rainbow print
-//learn how to make a .deb package
+//anitrendz-cli uses anitop api for fearless concurrency on the terminal 
