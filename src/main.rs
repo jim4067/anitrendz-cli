697b3eac32cbdb4ac54affd0b93edbab6831388a
jim4067 <jimmyimpulse2@gmail.com>
2021-07-03 21:50:36 +0300

    :contrutcion: :tada: finish this crate for running the cli app :tada:

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,57 @@
+extern crate clap;
+extern crate rand;
 extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 extern crate tokio;
 
-mod network;
+mod anime_net;
 mod app;
+mod music_net;
+mod utils;
+
+use clap::{App, Arg};
 
 fn main() {
-    // println!("let us print the title of the top anime");
-    // for anime in data.iter() {
-    //     println!(" image {}", anime.imageUrl);
-    //     println!(" peak {}", anime.stats.peak);
-    //     println!(" previously {}", anime.stats.previously);
-    //     println!(" stat {}", anime.stats.stat);
-    //     println!(" status {}", anime.stats.status);
-    // }
-    app::top_anime_list();
+    run();
 }
 
-//see if when i get the data I can sterilize it and change some of the variable names to use snake case
-//you could add a rank field and learn how to calculate it
+fn run() {
+    let cli = App::new("anitrendz-cli")
+        .version("0.1-beta\n")
+        .author("James M. <jimmyimpulse2@gmail.com>\n")
+        .about("Get a list of top anime, music charts and random recommendation right on your terminal")
+        .arg(
+            Arg::with_name("anime")
+                .short("a")
+                .long("anime-list")
+                .help("Print a list of top anime")
+                // .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("music")
+                .short("m")
+                .long("music-charts")
+                .help("get the list of the music charts"),
+        )       
+        .arg(
+            Arg::with_name("anime-recommendation").long("anime-rec").help("Recommends a random anime"),
+        )
+        .arg(
+            Arg::with_name("song-recommendation").short("s").long("song-rec").help("Recommends a random song"),
+        )
+        .get_matches();
+
+    // I want the default one to be for listing anime
+    if cli.is_present("anime") {
+        app::top_anime_list();
+    } else if cli.is_present("music") {
+        app::music_charts_list();
+    } else if cli.is_present("anime-recommendation") {
+        app::gen_random_anime();
+    } else if cli.is_present("song-recommendation") {
+        app::gen_random_song();
+    } else {
+        app::top_anime_list();
+    }
+}
