f4eac9e14f7f419ace2322d2a2fbf4dc80391d19
jim4067 <jimmyimpulse2@gmail.com>
2021-07-03 18:13:46 +0300

    :rocket: :construction: add function to recommend a random anime and song :rocket: :construction:

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,10 @@
 #![allow(non_snake_case, dead_code)]
 
+use rand::Rng;
+use std::f64::consts::PI;
+
 use crate::anime_net;
 use crate::music_net;
-use std::f64::consts::PI;
 
 fn rgb(freq: f64, spread: f64, i: f64) -> (u8, u8, u8) {
     let j = i / spread;
@@ -185,6 +187,94 @@ pub fn music_charts_list_uncolored() {
         println!();
     }
 }
+
+//generating random anime to watch
+//would you like a random recommendation
+pub fn gen_random_anime() {
+    let res = anime_net::get_anime();
+
+    let num = rand::thread_rng().gen_range(0..res.totalItems) as usize;
+
+    let anime = &res.data[num];
+    rainbow_println("\n A random recommendation of anime to watch \n", 0.1, 3.0);
+    rainbow_println(format!(" title: {}", anime.title).as_str(), 0.1, 3.0);
+    rainbow_println(format!(" studio: {}", anime.studio).as_str(), 0.1, 3.0);
+    rainbow_println(format!(" rank: {}", num + 1).as_str(), 0.1, 3.0);
+    rainbow_println(
+        format!(
+            " stats for nerds : [ peak: {} | previously: {} | stat: {} | status: {} | weeks_on_top: {} ] ",
+            anime.stats.peak,
+            anime.stats.previously,
+            anime.stats.stat,
+            anime.stats.status,
+            anime.stats.weeksOnTop
+        )
+        .as_str(),
+        0.1,
+        3.0,
+    );
+}
+
+pub fn gen_random_song() {
+    let res = music_net::get_music();
+
+    let num = rand::thread_rng().gen_range(0..res.totalItems) as usize;
+
+    let song = &res.data[num];
+    rainbow_println("\n A random recommendation a song to listen to \n", 0.1, 3.0);
+
+    rainbow_println(format!(" title: {}", song.title).as_str(), 0.1, 3.0);
+    rainbow_println(format!(" artists: {:?}", song.artists).as_str(), 0.1, 3.0);
+    rainbow_println(format!(" rank: {}", song.rank).as_str(), 0.1, 3.0);
+    rainbow_println(
+        format!(
+            " stats for nerds :: [ peak: {} | previously: {} | weeks_on_top: {} ]",
+            song.stats.peak, song.stats.previously, song.stats.weeks
+        )
+        .as_str(),
+        0.1,
+        3.0,
+    );
+
+    //printing where you can listen to the songs and handling the NONE values
+    rainbow_println(
+        format!(
+            " listen: youtube -> {}",
+            song.mediaUrl
+                .youtube
+                .as_ref()
+                .unwrap_or(&"404 Not available".to_string())
+        )
+        .as_str(),
+        0.1,
+        3.0,
+    );
+    rainbow_println(
+        format!(
+            " \t spotify -> {}",
+            song.mediaUrl
+                .spotify
+                .as_ref()
+                .unwrap_or(&"404 Not available".to_string())
+        )
+        .as_str(),
+        0.1,
+        3.0,
+    );
+    rainbow_println(
+        format!(
+            "\t itunes -> {}",
+            song.mediaUrl
+                .itunes
+                .as_ref()
+                .unwrap_or(&"404 Not available".to_string())
+        )
+        .as_str(),
+        0.1,
+        3.0,
+    );
+}
+
 //for the cli application. only this will be required in main
 fn run() {}
 
