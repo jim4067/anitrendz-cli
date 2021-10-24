5f8ca945fa550f888b3d1c05f1afacd08dea208a
jim4067 <jimmyimpulse2@gmail.com>
2021-10-24 15:24:19 +0300

    :sparkles: function to print the result to the screen

---
+++ src/display.rs
@@ -0,0 +1,25 @@
+use crate::anime_chart;
+use crate::net;
+
+pub async fn print_anime_charts() {
+    let anime_list = anime_chart::Header::chart_details(net::anime_charts_offline()).await;
+
+    println!(
+        "\n\t\t\t\t\t [ {} | {} | {} ]\n",
+        anime_list.season, anime_list.week, anime_list.date
+    );
+
+    for anime in anime_list.top_anime.iter().rev() {
+        println!(" title: {}", anime.title);
+        println!(" rank: {}", anime.rank);
+        println!(" studio: {}", anime.studio);
+        println!(" stats for nerd : [ peak: {} | previously: {} | stat: {} | status: {} | weeks_on_top: {} ] \n", anime.stats.peak, anime.stats.prev,anime.stats.stat, anime.stats.status, anime.stats.weeks_on_top );
+    }
+    
+    println!(
+        "\n\t\t\t\t\t [ {} | {} | {} ]\n",
+        anime_list.season, anime_list.week, anime_list.date
+    );
+}
+
+//this is teh crate that is responsible for displaying printing the results to the terminal
