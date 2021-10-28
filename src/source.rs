c0e6bbd90c08be703305ce0f2b544d50263acdfd
jim4067 <jimmyimpulse2@gmail.com>
2021-10-28 20:08:05 +0300

    :sparkle: new sources to include data html files for scrapping

--- src/source.rs
+++ src/source.rs
@@ -1,10 +1,21 @@
-// pub fn online(){}
-pub fn download_anime_charts(){}
+//getting anime charts(){}
+pub fn download_anime_charts() {}
 
 pub fn anime_charts_offline() -> &'static str {
-    include_str!("source.html")
+    include_str!("top_anime_source.html")
 }
 
+pub fn male_characters_offline() -> &'static str {
+    include_str!("male_characters.html")
+}
 
-//this crate is responsible for downloading the html source for the anitrendz.net or using a locally 
+pub fn female_characters_offline() -> &'static str {
+    include_str!("female_characters.html")
+}
+
+pub fn couple_ships_offline() -> &'static str {
+    include_str!("coupple.html")
+}
+
+//this crate is responsible for downloading the html source for the anitrendz.net or using a locally
 //downloaded version if the network is not available
