27d12623d9f6abf5a8f03b9d95d06ad7a11ef96d
jim4067 <jimmyimpulse2@gmail.com>
2021-11-05 09:57:10 +0300

    finish the display module

--- src/display.rs
+++ src/display.rs
@@ -1,11 +1,12 @@
-use crate::anime_chart;
-use crate::character_chart;
+#![allow(dead_code)]
+use crate::anime_scrapper;
+use crate::generic_scrapper;
 use crate::source;
 
 pub fn print_anime_charts() {
     let base_url = "https://anitrendz.net/charts/top-anime/";
     let source = source::download_html_source(base_url);
-    let anime_list = anime_chart::Header::chart_details(source);
+    let anime_list = anime_scrapper::Header::chart_details(source);
 
     println!(
         "\n\t\t\t\t\t [ {} | {} | {} ]\n",
@@ -19,7 +20,7 @@ pub fn print_anime_charts() {
         println!(" stats for nerd : [ peak: {} | previously: {} | stat: {} | status: {} | weeks_on_top: {} ] \n", anime.stats.peak, anime.stats.prev,anime.stats.stat, anime.stats.status, anime.stats.weeks_on_top );
     }
     println!(
-        "\n\t\t\t\t\t [ {} | {} | {} ]\n",
+        "\n\t\t\t\t [ {} | {} | {} ]\n",
         anime_list.season, anime_list.week, anime_list.date
     );
 }
@@ -27,14 +28,14 @@ pub fn print_anime_charts() {
 pub fn print_male_character_charts() {
     let base_url = "https://anitrendz.net/charts/male-characters/";
     let source = source::download_html_source(base_url);
-    let character_list = character_chart::Header::chart_details(source);
+    let character_list = generic_scrapper::Header::chart_details(source);
 
     println!(
         "\n\t\t\t [ {} | {} | {} | {} ]\n",
         character_list.title, character_list.season, character_list.week, character_list.date
     );
 
-    for male_character in character_list.characters.iter().rev() {
+    for male_character in character_list.chart_entry.iter().rev() {
         println!(" name: {}", male_character.name);
         println!(" show: {} ", male_character.show);
         println!(" rank: {} ", male_character.rank);
@@ -50,14 +51,14 @@ pub fn print_male_character_charts() {
 pub fn print_female_character_charts() {
     let base_url = "https://anitrendz.net/charts/female-characters/";
     let source = source::download_html_source(base_url);
-    let character_list = character_chart::Header::chart_details(source);
+    let character_list = generic_scrapper::Header::chart_details(source);
 
     println!(
         "\n\t\t\t [ {} | {} | {} | {} ]\n",
         character_list.title, character_list.season, character_list.week, character_list.date
     );
 
-    for female_character in character_list.characters.iter().rev() {
+    for female_character in character_list.chart_entry.iter().rev() {
         println!(" name: {}", female_character.name);
         println!(" show: {} ", female_character.show);
         println!(" rank: {} ", female_character.rank);
@@ -73,14 +74,14 @@ pub fn print_female_character_charts() {
 pub fn print_couple_ships() {
     let base_url = "https://anitrendz.net/charts/couple-ship/";
     let source = source::download_html_source(base_url);
-    let character_list = character_chart::Header::chart_details(source);
+    let character_list = generic_scrapper::Header::chart_details(source);
 
     println!(
         "\n\t\t\t [ {} | {} | {} | {} ]\n",
         character_list.title, character_list.season, character_list.week, character_list.date
     );
 
-    for couple_ships in character_list.characters.iter().rev() {
+    for couple_ships in character_list.chart_entry.iter().rev() {
         println!(" name: {}", couple_ships.name);
         println!(" show: {} ", couple_ships.show);
         println!(" rank: {} ", couple_ships.rank);
@@ -90,6 +91,52 @@ pub fn print_couple_ships() {
     println!(
         "\n\t\t\t [ {} | {} | {} | {} ]\n",
         character_list.title, character_list.season, character_list.week, character_list.date
+    );
+}
+
+pub fn print_ending_songs() {
+    let base_url = "https://anitrendz.net/charts/ed-theme-songs/";
+    let source = source::download_html_source(base_url);
+    let song_list = generic_scrapper::Header::chart_details(source);
+
+    println!(
+        "\n\t\t\t [ {} | {} | {} | {} ]\n",
+        song_list.title, song_list.season, song_list.week, song_list.date
+    );
+
+    for songs in song_list.chart_entry.iter().rev() {
+        println!(" name: {}", songs.name);
+        println!(" show: {} ", songs.show);
+        println!(" rank: {} ", songs.rank);
+        println!(" stats for nerd : [ peak: {} | previously: {} | stat: {} | status: {} | weeks_on_top: {} ] \n", songs.stats.peak, songs.stats.prev, songs.stats.stat, songs.stats.status, songs.stats.weeks_on_top );
+    }
+
+    println!(
+        "\n\t\t\t [ {} | {} | {} | {} ]\n",
+        song_list.title, song_list.season, song_list.week, song_list.date
+    );
+}
+
+pub fn print_opening_songs() {
+    let base_url = "https://anitrendz.net/charts/op-theme-songs/";
+    let source = source::download_html_source(base_url);
+    let song_list = generic_scrapper::Header::chart_details(source);
+
+    println!(
+        "\n\t\t\t [ {} | {} | {} | {} ]\n",
+        song_list.title, song_list.season, song_list.week, song_list.date
+    );
+
+    for songs in song_list.chart_entry.iter().rev() {
+        println!(" name: {}", songs.name);
+        println!(" show: {} ", songs.show);
+        println!(" rank: {} ", songs.rank);
+        println!(" stats for nerd : [ peak: {} | previously: {} | stat: {} | status: {} | weeks_on_top: {} ] \n", songs.stats.peak, songs.stats.prev, songs.stats.stat, songs.stats.status, songs.stats.weeks_on_top );
+    }
+
+    println!(
+        "\n\t\t\t [ {} | {} | {} | {} ]\n",
+        song_list.title, song_list.season, song_list.week, song_list.date
     );
 }
 
