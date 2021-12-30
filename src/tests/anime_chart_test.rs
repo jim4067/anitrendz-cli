2d27d4dc2e9bf9229df669d5994f558916d99548
jim4067 <jimmyimpulse2@gmail.com>
2021-12-30 16:16:55 +0300

    fixed failing test

--- src/tests/anime_chart_test.rs
+++ src/tests/anime_chart_test.rs
@@ -1,12 +1,16 @@
-use crate::anime_chart;
+use crate::anime_scrapper;
 use crate::source;
 
 #[test]
 //After the tenth entry, weeks_on_top for other entries is not empty. Instead, I hardcoded a string val of zero
 //so making sure that it is there
 fn weeks_on_top_not_empty() {
-    let html = source::anime_charts_offline();
-    for val in anime_chart::TopAnime::get_anime_charts(html){
-        assert!(!val.stats.weeks_on_top.is_empty())
+    // let html = source::anime_charts_offline();
+    let base_url = "https://anitrendz.net/charts/male-characters/";
+    let source = source::download_html_source(base_url);
+    let anime_list = anime_scrapper::Header::chart_details(source);
+
+    for val in anime_list.top_anime.iter() {
+        assert!(!val.stats.weeks_on_top.is_empty());
     }
 }
