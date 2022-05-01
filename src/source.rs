9aa66af10a9c481f14767e900de9536f7799779b
jim4067 <jimmyimpulse2@gmail.com>
2022-05-01 14:21:19 +0300

    comment out offline charts -> they cause errors

--- src/source.rs
+++ src/source.rs
@@ -6,21 +6,21 @@ pub fn download_html_source(base_url: &str) -> String {
 }
 
 //Getting local source files
-pub fn anime_charts_offline() -> String {
-    include_str!("data/top_anime_source.html").to_string()
-}
+// pub fn anime_charts_offline() -> String {
+//     include_str!("data/top_anime_source.html").to_string()
+// }
 
-pub fn male_characters_offline() -> String {
-    include_str!("data/male_characters.html").to_string()
-}
+// pub fn male_characters_offline() -> String {
+//     include_str!("data/male_characters.html").to_string()
+// }
 
-pub fn female_characters_offline() -> String {
-    include_str!("data/female_characters.html").to_string()
-}
+// pub fn female_characters_offline() -> String {
+//     include_str!("data/female_characters.html").to_string()
+// }
 
-pub fn couple_ships_offline() -> String {
-    include_str!("data/coupple.html").to_string()
-}
+// pub fn couple_ships_offline() -> String {
+//     include_str!("data/coupple.html").to_string()
+// }
 
 //this crate is responsible for downloading the html source for the anitrendz.net or using a locally
 //downloaded version if the network is not available
